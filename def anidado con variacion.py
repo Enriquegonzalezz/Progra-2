f045fb483bc3bb0b7823f2eac75e589acbafc713
Enriquegonzalezz <141170095+Enriquegonzalezz@users.noreply.github.com>
2023-08-08 23:52:29 -0400

    Add files via upload

---
+++ def anidado con variacion.py
@@ -0,0 +1,18 @@
+
+#hacer una funcion dentro de otra funcion  
+#hacemos la primera funcion 
+def funcionenvolvente():
+    haisa = 85 # le damos un valor 
+    def funcioninterior(): # llamamos a la segunda
+        nonlocal haisa   #para agregar un cambio a la variable utilizamos nonlocal 
+        haisa = haisa + 5  #agregamos el cambio
+        return haisa #imrprimimos
+    return funcioninterior() #retornamos la funcion secundaria
+
+funcionenvolvente() #llamamos a la funcion original
+    
+
+    
+
+
+    
